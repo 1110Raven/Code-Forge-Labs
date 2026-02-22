ab8e55f2435b4aef9a5b08b6bdfb8cb7b95aea9f
1110Raven <165486694+1110Raven@users.noreply.github.com>
2026-02-22 15:31:08 +0000

    Refactor greeting logic into a function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 print("this is main file of the project")
 # the main file has been updated
-name="Amir"
-print("Hello "+name)
-print("Welcome "+name)
-print("Have a good day "+name)
+def greet(name):
+    print(f"Hello {name}")
+    print(f"Welcome {name}")
+    print(f"Have a good day {name}")
+
+greet("Amir")
