955434c9854593b494c5e82dc997fbb8d4d57160
Amir <amirtorabi759@gmail.com>
2026-02-23 12:13:31 +0000

    Refactor: simplify greet function

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 print("this is main file of the project")
 # the main file has been updated
-def greet(name, age):
+def greet(name):
     print(f"Hello {name}")
     print(f"Welcome {name}")
     print(f"Have a good day {name}")
-    print(f"you are {age} years old")
 
-greet("Amir", 32)
+greet("Amir")
 # Updated from local cloned repository
